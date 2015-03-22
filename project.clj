e7a4de4e16d6f4f5205983dc2c1186ff26bcd47d
Hugo Firth <hugo.firth@gmail.com>
2015-03-22 11:25:13 +0300

    updated some project information

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject simple-queue "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url ""
+(defproject org.clojars.firthh/simple-queue "0.1.0-SNAPSHOT"
+  :description "Simple wrapper around Langohr"
+  :url "https://github.com/firthh/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
